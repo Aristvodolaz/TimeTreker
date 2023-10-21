48286cc1fdf70410767589ee87bf1b7b01998417
Aristvodolaz <kap.moral@mail.ru>
2023-10-21 19:36:34 +0300

    {6} - add splash loader

--- app/src/main/java/com/example/timetrekerforandroid/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/timetrekerforandroid/activity/SplashScreenActivity.java
@@ -1,16 +1,33 @@
 package com.example.timetrekerforandroid.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-import androidx.annotation.Nullable;
+import com.example.timetrekerforandroid.R;
 
 public class SplashScreenActivity extends Activity {
-    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_loader_layout);
 
+        ImageView splashImageView = findViewById(R.id.splash_image);
 
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
+        splashImageView.startAnimation(fadeInAnimation);
+
+        new android.os.Handler().postDelayed(
+                () -> {
+                    Intent intent = new Intent(this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                },
+                3000
+        );
     }
 }
