7dfcd1ce9aed42950062916479b44ff463e48d42
Sanchosszza <zgonnikova00@gmail.com>
2023-10-21 22:12:26 +0300

    user profile and exit

--- app/src/main/java/com/example/timetrekerforandroid/fragment/user/UserStartFragment.java
+++ app/src/main/java/com/example/timetrekerforandroid/fragment/user/UserStartFragment.java
@@ -1,12 +1,26 @@
 package com.example.timetrekerforandroid.fragment.user;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 
 import com.example.timetrekerforandroid.R;
 import com.example.timetrekerforandroid.fragment.BaseFragment;
 
+import butterknife.BindView;
+
 public class UserStartFragment extends BaseFragment {
     public static final String LOGIN ="login";
+
+    @BindView(R.id.exit)
+    SurfaceView exitBtn;
+
     public static UserStartFragment newInstance(String login) {
         Bundle args = new Bundle();
         args.putString(LOGIN, login);
@@ -14,6 +28,28 @@ public class UserStartFragment extends BaseFragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        exitBtn.setOnClickListener(l->{
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(getContext());
+            mBuilder.setTitle("Подтверждение");
+            mBuilder.setMessage("Вы уверены что хотите выйти?");
+            mBuilder.setPositiveButton("Да", null);
+            mBuilder.setNegativeButton("Нет", null);
+            mBuilder.show();
+
+            Button mPositiveButton = mBuilder.create().getButton(AlertDialog.BUTTON_POSITIVE);
+            mPositiveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.exit(0);
+                }
+            });
+        });
+    }
+
     @Override
     protected int layoutId() {
         return R.layout.user_fragment;
